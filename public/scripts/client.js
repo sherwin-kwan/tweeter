45122d0b4748a6a43d1b7dc1336fcfbe56140470
Sherwin <sherwin.kwan92@gmail.com>
2020-10-06 22:34:38 -0700

    tweets now load client-side from a data object

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,28 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-/* function to produce HTML for chirps */
+/* functions to produce HTML for chirps */
 
-/*
-<article>
-  <header>
-    <div class="name"><img class="avatar" height="50px" width="50px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Abraham_Lincoln_O-77_matte_collodion_print.jpg/800px-Abraham_Lincoln_O-77_matte_collodion_print.jpg">Abraham Lincoln</div>
-    <address>@honestabe</address>
-  </header>
-  <p>
-    Four score and seven years ago our fathers brought forth, upon this continent, a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal.
-    And two dozen and one years ago, I started slaying vampires.
-  </p>
-  <footer>
-    <time>157 years ago</time>
-    <figure>
-      <span class="emoji">🔁</span>
-      <span class="emoji">💖</span>
-      <span class="emoji">🙋</span>
-    </figure>
-  </footer>
-</article>*/
-
+// Takes a single chirp object formatted in JSON from the database, and returns a jQuery object containing HTML markup to display the chirp
 const createChirpElement = (chirpContent) => {
   const avatar = chirpContent.user.avatars;
   const name = chirpContent.user.name;
@@ -35,7 +16,7 @@ const createChirpElement = (chirpContent) => {
   return $(`
   <article>
   <header>
-    <div class="name"><img class="avatar" src="${avatar}">${name}</div>
+    <div class="name"><img class="avatar" width="50px" height="50px" src="${avatar}">${name}</div>
     <address>${handle}</address>
   </header>
   <p>
@@ -52,19 +33,7 @@ const createChirpElement = (chirpContent) => {
       `);
 };
 
-const testObject1 = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png"
-    ,
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
-
+// This is a temporary storage for the data until the in-memory database can be linked properly
 const data = [
   {
     "user": {
@@ -82,15 +51,57 @@ const data = [
     "user": {
       "name": "Descartes",
       "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
+      "handle": "@rd"
+    },
     "content": {
       "text": "Je pense , donc je suis"
     },
     "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Abraham Lincoln",
+      "avatars": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Abraham_Lincoln_O-77_matte_collodion_print.jpg/800px-Abraham_Lincoln_O-77_matte_collodion_print.jpg",
+      "handle": "@HonestAbe"
+    },
+    "content": {
+      "text": "Four score and seven years ago our fathers brought forth, upon this continent, a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal.\
+      And three dozen years ago, I started hunting vampires."
+    },
+    "created_at": 0
+  },
+  {
+    "user": {
+      "name": "George Harrison",
+      "avatars": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/George_Harrison_1974.jpg/690px-George_Harrison_1974.jpg",
+      "handle": "@GeorgeH"
+    },
+    "content": {
+      "text": "Here comes the sun, and I say it's alright #harekrishna"
+    },
+    "created_at": 0
+  },
+  {
+    "user": {
+      "name": "Jules",
+      "avatars": "https://i.pinimg.com/originals/38/80/be/3880be0b24e775a7f47402687421d533.jpg",
+      "handle": "@royalewithcheese"
+    },
+    "content": {
+      "text": "Say 'what' again. Say 'what' again, I dare you, I double dare you motherfucker, say what one more Goddamn time!"
+    },
+    "created_at": 778377600
   }
 ]
 
-$(function() {
-  const $chirp = $('#listOfChirps');
-  $chirp.append(createChirpElement(testObject1));
-})
+// Renders a series of chirps with jQuery
+const renderChirps = function (arrOfChirps) {
+  arrOfChirps.forEach((chirp) => {
+    $('#listOfChirps').append(createChirpElement(chirp));
+  });
+};
+
+// Renders chirps on document ready
+$(function () {
+  renderChirps(data);
+});
