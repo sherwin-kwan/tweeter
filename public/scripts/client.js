0003eea15eb7b087d7e7ee5f9f205dc55e7c1cb5
Sherwin <sherwin.kwan92@gmail.com>
2020-10-08 15:45:09 -0700

    button now shows/hides the tweet writing area

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,26 +2,50 @@
 $(function () {
   // RUNS ON DOCUMENT READY
 
+  // CONSTANTS
   // Define variables (with $ to signify jQuery object) for key nodes in the DOM
   const $chirpTextArea = $('#chirpTextArea');
   const $counter = $chirpTextArea.next('.submitDiv').find('.counter');
   const $chirpForm = $('#chirpForm');
+  const $composeButton = $('nav button');
+  const $formWrapper = $('#formWrapper');
+  //
+  //
 
 
+  // ON INITIAL PAGE LOAD
   // Render chirps for the first time
   load_chirps();
 
   // Initialize placeholder message for the textarea
   $chirpTextArea[0].placeholder = 'Friendship ended with Twitter. Now Chirper is my best friend';
+
   // Initialize the counter to 140 characters
   changeCounter($chirpTextArea, $counter, maxChirpLength);
+  //
+  //
+
 
 
   // EVENT LISTENERS
-  // Be ready to listen for inputs in the textarea and change the counter number
-  $chirpTextArea.on('input', function (event) {
+
+
+  // 1) Listen for clicking the button to compose new chirp
+  $composeButton.on('click', function(event) {
+    // Up/down sliding motion to reveal the textarea
+    if ($formWrapper.is(':visible')) {
+      $formWrapper.slideUp(500);
+    } else {
+      $formWrapper.slideDown(500);
+    }
+  })
+
+  // 2) Listen for inputs in the textarea and change the counter number
+  $chirpTextArea.on('input', (event) => {
     changeCounter($chirpTextArea, $counter, maxChirpLength);
   });
+
+  // 3) Listen for submission of new chirps
   $chirpForm.on('submit', function (event) {
     // Pass two arguments to form_submit function: the event, and the jQuery wrapper for the new chirp form
     try { // In case validation errors happen
@@ -32,7 +56,6 @@ $(function () {
       $(this).find('.submitDiv p').text(err.message);
     };
   });
-
 
 });
 
