e8641d9ef66be3743774156a4146d52def099d79
Sherwin <sherwin.kwan92@gmail.com>
2020-10-07 17:59:16 -0700

    fixed an asynchronous code issue

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,15 +6,23 @@
 
 /* LOADING CHIRPS ONTO THE PAGE */
 
-const escape = require('./helpers.js').escapeChars;
+// Helper function which escapes unwanted characters in a string to prevent script injections
+// It borrows the jQuery "createTextNode" function (used for sanitizing the contents of HTML nodes), by creating
+// a dummy div tag to encase the string. The div is not actually output as HTML.
+const escapeChars =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
 // Takes a single chirp object formatted in JSON from the database, and returns a jQuery object containing HTML markup to display the chirp
 const createChirpElement = (chirpContent) => {
   const avatar = chirpContent.user.avatars;
-  const name = escape(chirpContent.user.name);
-  const handle = escape(chirpContent.user.handle);
+  const name = escapeChars(chirpContent.user.name);
+  const handle = escapeChars(chirpContent.user.handle);
   const time = chirpContent.created_at;
-  const chirpText = escape(chirpContent.content.text);
+  const chirpText = escapeChars(chirpContent.content.text);
+  console.log(name, handle, chirpText);
   return $(`
   <article>
   <header>
@@ -47,38 +55,47 @@ const form_submit = (event, $form) => {
   // Begin with validating the input
   const chirpLength = $form.find('textarea').val().length;
   console.log(chirpLength);
+  // Errors if chirps are blank or too long.
   if (chirpLength > 300) {
     throw new Error(`Your message is too long. Please shorten your chirp and try again.`);
   } else if (chirpLength === 0) {
     throw new Error('Please type a chirp in the text area provided');
   };
-  console.log($form.serialize()); // This is unsafe as the form could contain chracters used for XSS
+  console.log($form.serialize());
+  // Note: No validation on the input side, people can put <script> tags into the database if they want. Data is only sanitized when output and 
+  // rendered onto the page.
   $.ajax('/tweets/', { 
     method: 'POST', 
     data: $form.serialize()
    })
-    .then(console.log('Posted'))
+    .then(load_chirps)
     .fail((xhr, status, err) => {
       console.log(status, err);
     })
-}
+};
 
+// Loads the chirps asynchronously from database
 const load_chirps = () => {
   $.ajax('/tweets/', {method: 'GET'})
   .then((res) => renderChirps(res))
   .fail((xhr, status, err) => {
     console.log(status, err);
   })
-}
+};
 
 $(function () {
   // Renders chirps on document ready
   load_chirps();
   // Handler for submitting the new chirp form.
   const $chirpForm = $('#chirpForm');
-  $chirpForm.submit(function (event) {
+  $chirpForm.on('submit', function (event) {
     // Pass two arguments to form_submit function: the event, and the jQuery wrapper for the new chirp form
-    form_submit(event, $(this));
-    load_chirps();
+    try { // In case validation errors happen
+      console.log('Attempting to submit form');
+      form_submit(event, $(this));
+    } catch (err) {
+      console.log('Error');
+      $(this).find('.submitDiv p').text(err.message);
+    };
   });
 });
